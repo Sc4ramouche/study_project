c0d6b8f5cf0e15274785b0bc03ce82dbf1651187
Vladislav Kovechenkov <Vladislav.Kovechenkov@gmail.com>
2017-12-28 15:10:52 +0300

    added news page

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -2,6 +2,127 @@
 
 @section('content')
 
-<h1>News</h1>
+<section class="catalog-links">
+  <div class="container">
+    <nav class="catalog-navigation">
+      <ul class="catalog-list">
+        <li><a href="/catalog/1">Посуда для приготовления</a></li>
+        <li><a href="/catalog/2">Посуда для сервировки</a></li>
+        <li><a href="/catalog/3">Хранение на кухне</a></li>
+        <li><a href="/catalog/4">Кухонная утварь</a></li>
+      </ul>
+    </nav>
+  </div>
+</section>
+
+<div class="container">
+  <ul class="breadcrumb">
+    <li><a href="#">Главная</a></li>
+    <li>Новости</li>
+  </ul>
+</div>
+
+<section class="news-page">
+  <div class="container news-page-container">
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+    <div class="newspage-item">
+      <div class="news-img">
+        <img src="img/news-image.png" alt="news">
+      </div>
+      <div class="news-content-container">
+        <time datetime="2017-11-12" class="news-date">12 ноября 2017</time>
+        <h2 class="news-header">Новая коллекция посуды от Luminarc Harena.</h2>
+        <p class="news-content">Белоснежная линейка столовой посуды
+          в греко-романском стиле придется по вкусу как любителям классики,
+          так и поклонникам современного дизайна.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+
 
 @endsection()
